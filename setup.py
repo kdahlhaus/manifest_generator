b8ec944ef1e85111e7ce3600802b34da92bab745
kpd <kpd@gmail.com>
2014-05-27 20:31:10 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "manifest_generator",
+    version = "1.0.0",
+    author = "Kevin Dahlhausen",
+    author_email = "kevin.dahlhausen@gmail.com",
+    description = ("A utility to smartly generate an HTML5 application cache manifest"),
+    license = "GPL3",
+    keywords = "html5 cache manifest",
+    url = "https://github.com/kdahlhaus/manifest_generator",
+    packages=['manifestgen',],
+    long_description="This package contains a utility to generate the HTML5 application cache manifest file. Running it multiple times only changes the manifest if the manifest contents or contents of files listed in the cache change.",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Environment :: Console",
+    ],
+    scripts=['manifest_gen']
+)
+
